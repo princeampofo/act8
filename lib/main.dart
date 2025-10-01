6a237ddbcb12777195933e8dc3ccb3915f4c2264
princeampofo <pla252@nyu.edu>
2025-10-01 18:36:53 -0400

    added theme switch

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,40 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FadingTextAnimation(),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: FadingTextAnimation(
+        isDarkMode: _isDarkMode,
+        onThemeToggle: toggleTheme,
+      ),
     );
   }
 }
 
 class FadingTextAnimation extends StatefulWidget {
+  final bool isDarkMode;
+  final VoidCallback onThemeToggle;
+
+  FadingTextAnimation({required this.isDarkMode, required this.onThemeToggle});
+
   @override
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
@@ -32,15 +56,40 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Fading Text Animation'),
+        actions: [
+          IconButton(
+            icon: Icon(widget.isDarkMode ? Icons.nightlight_round: Icons.wb_sunny ),
+            onPressed: widget.onThemeToggle,
+          ),
+        ],
       ),
       body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: Duration(seconds: 1),
+              child: Text(
+                'Hello, Flutter!',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Light'),
+                Switch(
+                  value: widget.isDarkMode,
+                  onChanged: (value) {
+                    widget.onThemeToggle();
+                  },
+                ),
+                Text('Dark'),
+              ],
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
